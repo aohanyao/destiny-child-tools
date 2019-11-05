0f61e526ca29b1e9ec5965d207a7829ffc8cad36
LokiCoder <lokicoder@gmail.com>
2019-11-05 18:05:57 -0500

    Add more tests
    Signed-off-by: LokiCoder <lokicoder@gmail.com>

--- cypress/integration/tests.spec.js
+++ cypress/integration/tests.spec.js
@@ -9,4 +9,9 @@ describe('cypress/integration/tests.spec.js', () => {
     cy.contains('Childs & Mods Database').click()
     cy.contains('View as')
   })
+  it('individual child', () => {
+    cy.visit('/childs/c000')
+    cy.title().should('include', 'Hero')
+    cy.contains('Hero')
+  })
 })
