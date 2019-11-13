86537d4266d2ae6c77b3c80c576baf6ae743a484
LokiCoder <lokicoder@gmail.com>
2019-11-13 12:45:40 -0500

    Fix preview generation with special characters

--- cypress/integration/previews.spec.js
+++ cypress/integration/previews.spec.js
@@ -3,10 +3,11 @@ import missingPreviews from '../../missing-previews.json'
 describe('cypress/integration/tests.spec.js', () => {
   missingPreviews.forEach(key => {
     it(key, () => {
+      const encodedKey = encodeURIComponent(key)
       // ['424242', 'ffffff', '000000'].forEach(color => {
-      cy.request(`/live2d/assets/${key}/MOC.${key}.json`).then(() => {
+      cy.request(`/live2d/assets/${encodedKey}/MOC.${encodedKey}.json`).then(() => {
         ['424242'].forEach(color => {
-          cy.visit(`/live2d/viewer.html?mN=${key}&size=1000&background=%23${color}`)
+          cy.visit(`/live2d/viewer.html?mN=${encodedKey}&size=1000&background=%23${color}`)
           cy.wait(3000)
           cy.get('canvas').screenshot(key + '-' + color, {padding: -20})
         })
