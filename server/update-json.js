5f848baf188a7a287ed77b8a650f2f3e4b157875
LokiCoder <lokicoder@gmail.com>
2019-11-14 08:09:10 -0500

    Calculate nuof mods per child during dist build
    This is in support of #3 and #4.

---
+++ server/update-json.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import path from 'path'
+import childs from '../docs/data/childs.json'
+import mods from '../docs/data/mods.json'
+
+Object.keys(childs).forEach(id => {
+  const childMods = mods.filter(({child}) => child == id)
+  childs[id].numMods = childMods.length
+})
+
+fs.writeFileSync(path.join(__dirname, '../docs/data/childs.json'), JSON.stringify(childs, null, 2))
