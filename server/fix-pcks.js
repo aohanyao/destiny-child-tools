ea73ca018bf2cfe14754ca575e3872f78ada1d18
LokiCoder <lokicoder@gmail.com>
2019-11-18 12:11:04 -0500

    Clean up unused code
    Signed-off-by: LokiCoder <lokicoder@gmail.com>

--- server/fix-pcks.js
+++ server/fix-pcks.js
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import childs from '../docs/data/childs.json'
 import {execSync} from 'child_process'
 
 const assetsPath = path.resolve(__dirname, '../docs/live2d/assets/')
@@ -49,37 +48,3 @@ fs.readdirSync(assetsPath).forEach(file => {
     });
   }
 })
-
-
-// console.log('Importing characters ...')
-// fs.readdirSync(path.resolve(__dirname, '../import/character')).forEach(file => {
-//   const parts = file.match(/([a-z]+\d{3})_(\d\d)\.pck$/)
-//   if(parts) {
-//     const id = parts[1],
-//           variant = parts[2],
-//           child = childs[id]
-//     if(!child) {
-//       childs[id] =  {
-//         id,
-//         name: '?',
-//         variants: {}
-//       }
-//     }
-//     if(!childs[id].variants[variant]) {
-//       childs[id].variants[variant] = {
-//         title: '',
-//         positions: {home: {x: -15, y: -75, scale: 0.75}}
-//       }
-//     }
-//     console.log(`\nExtracing Live2d data for ${file} ${childs[id].name}`)
-//     run('./pckmanager/PCK.exe /L ./import/character/' + file)
-//     run(`rm -rf ./docs/live2d/assets/${id}_${variant}`)
-//     run(`mv -f ./import/character/${id}_${variant} ./docs/live2d/assets/${id}_${variant}`)
-//     run('./pckmanager/PCK.exe /R /U /C ./import/character/' + file)
-//     run(`rm -rf ./import/character/${id}_${variant}`)
-//     run(`mv -f ./import/character/${file}.newCompressedUnencrypted ./docs/live2d/assets/${id}_${variant}/${file}`)
-//     run('rm ./import/character/' + file)
-//   }
-// })
-
-// fs.writeFileSync(path.resolve(__dirname, '../docs/data/childs.json'), JSON.stringify(childs))
