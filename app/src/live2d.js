37e16caa8a81abc239776a03ecbae3fa022b32cb
LokiCoder <lokicoder@gmail.com>
2019-11-20 16:24:31 -0500

    Add report mod issue button
    Fixes #11

--- app/src/live2d.js
+++ app/src/live2d.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {
-  Text,
-  View
-} from 'react-native'
+import {View, ScrollView} from 'react-native'
 import {getStoragePermission} from './lib/permissions.js'
-import {Title, Button} from 'react-native-paper'
+import {Title, Button, Text} from 'react-native-paper'
 import RNFetchBlob from 'rn-fetch-blob'
 import RNFS from 'react-native-fs'
 import {clientPaths} from './lib/paths.js'
 import {WebView} from 'react-native-webview'
+import openUrl from './lib/open-url.js'
 
 const installMod = props => {
   const {id, pckName} = props
@@ -51,29 +49,46 @@ const installMod = props => {
 }
 
 const Live2D = props => {
-  const {id} = props,
-        pckName = id.match(/[a-z]{1,2}\d{3}_\d{2}/)[0]
+  const {id, pckName, child} = props,
+        matches = id.match(/[a-z]{1,2}\d{3}_\d{2}/),
+        newIssueTitle = encodeURIComponent(child.get('name') + ' mod issue for ' + id),
+        newIssueBody = encodeURIComponent(
+          `[enter details about the issue with ${child.get('name')} here]\n\n` +
+          `Leave this Mod ID here: ${id}\n\n` +
+          `Leave this link here: http://localhost:3000/destiny-child-tools/childs/${child.get('id')}/`)
   return (
-    <View padding={20} height={500}>
+    <ScrollView padding={20}>
       <Title>{id}</Title>
-      <WebView
-        source={{uri: `https://lokicoder.github.io/destiny-child-tools/live2d/viewer.html?mN=${id}&size=2000&background=%23424242`}}
-        automaticallyAdjustContentInsets={false}
-      />
+      <View height={400}>
+        <WebView
+          source={{uri: `https://lokicoder.github.io/destiny-child-tools/live2d/viewer.html?mN=${id}&size=2000&background=%23424242`}}
+          automaticallyAdjustContentInsets={false}
+        />
+      </View>
       <Button
         mode="contained"
         icon="cloud-download"
         onPress={() => installMod(Object.assign({}, props, {pckName})) } >
-        Install This Mod
+        {pckName == id ? 'Restore' : 'Install This Mod'}
       </Button>
-    </View>
+      <View style={{marginTop: 20, paddingBottom: 10}}>
+        <Button icon="bug" mode="outlined" color='white' onPress={() => openUrl(`https://github.com/LokiCoder/destiny-child-tools/issues/new?title=${newIssueTitle}&body=${newIssueBody}`)}>
+          Report Issue with this Mod
+        </Button>
+      </View>
+      <Text style={{color: '#aaa', textAlign: 'center'}}>(requires a GitHub account)</Text>
+    </ScrollView>
   )
 }
 
 export default connect(
   state => {
+    const id = state.get('view').get('id'),
+          matches = id.match(/([a-z]{1,2}\d{3})_\d{2}/)
     const props = {
-      id: state.get('view').get('id')
+      id,
+      pckName: matches[0],
+      child: state.get('data').get('childs').get(matches[1])
     }
     Object.keys(clientPaths).forEach(client => {
       props[client + 'Path'] = state.get('settings').get(client + 'Path')
